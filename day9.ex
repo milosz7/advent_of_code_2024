44ba4139f5c9875f6a3eddd805923eb862598729
Miłosz Bargieł <miloszbargiel@gmail.com>
2024-12-10 15:20:36 +0100

    Day 9.2 solution

--- day9.ex
+++ day9.ex
@@ -80,4 +80,79 @@ defmodule Solution do
     |> consume_sequence()
     |> calculate_checksum()
   end
+
+  def parse_data(data) do
+    data =
+      data
+      |> Enum.reduce({[], 0, 0}, fn {len, code}, {acc, idx, id} ->
+        case {len, code} do
+          {len, @free_space} ->
+            next_idx = idx + len
+            {[{@free_space_mark, idx, len} | acc], next_idx, id}
+
+          {len, @is_file} ->
+            next_idx = idx + len
+            {[{id, idx, len} | acc], next_idx, id + 1}
+        end
+      end)
+      |> elem(0)
+
+    disk_data =
+      data |> Enum.filter(fn {val, _, _} -> val != @free_space_mark end)
+
+    free_space =
+      data |> Enum.filter(fn {val, _, _} -> val == @free_space_mark end) |> Enum.reverse()
+
+    {disk_data, free_space}
+  end
+
+  def update_free_space(file, space, updated \\ :noupdate, acc \\ [])
+  def update_free_space(file, [], _, acc), do: {file, Enum.reverse(acc)}
+
+  def update_free_space(
+        {id, f_start, f_len} = file,
+        [{symbol, s_start, s_len} = space | rest],
+        :noupdate,
+        acc
+      ) do
+    if f_start > s_start and f_len <= s_len do
+      update_free_space({id, s_start, f_len}, rest, :update, [
+        {symbol, s_start + f_len, s_len - f_len} | acc
+      ])
+    else
+      update_free_space(file, rest, :noupdate, [space | acc])
+    end
+  end
+
+  def update_free_space(file, [fragment | rest], :update, acc) do
+    update_free_space(file, rest, :update, [fragment | acc])
+  end
+
+  def reorder_files(input, acc \\ [])
+
+  def reorder_files({[], _}, acc), do: acc
+
+  def reorder_files({[file | rest], free_space}, acc) do
+    {file, free_space} = update_free_space(file, free_space)
+    reorder_files({rest, free_space}, [file | acc])
+  end
+
+  def checksum_from_tuples(reordered_disk) do
+    reordered_disk
+    |> Enum.reduce(0, fn {id, start, len}, acc ->
+      sum =
+        for(num <- start..(start + len - 1), do: id * num)
+        |> Enum.sum()
+
+      acc + sum
+    end)
+  end
+
+  def solve_2(file_path) do
+    file_path
+    |> read_file()
+    |> parse_data()
+    |> reorder_files()
+    |> checksum_from_tuples()
+  end
 end
