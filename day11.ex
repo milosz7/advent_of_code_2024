1209bfffe5904fe78d47c24f51683d1a277cfb86
Miłosz Bargieł <miloszbargiel@gmail.com>
2024-12-12 13:40:57 +0100

    Day 11 improvements

--- day11.ex
+++ day11.ex
@@ -14,7 +14,7 @@ defmodule Solution do
   def n_digits(0), do: 1
   def n_digits(num), do: (num |> :math.log10() |> trunc()) + 1
 
-  def transform_stone(0), do: 1
+  def transform_stone(0), do: [1]
 
   def transform_stone(stone) do
     len = n_digits(stone)
@@ -26,30 +26,51 @@ defmodule Solution do
       right = rem(stone, divisor)
       [left, right]
     else
-      stone * 2024
+      [stone * 2024]
     end
   end
 
-  def transform_stones(stones, n_iter, current_iter \\ 0, acc \\ [])
+  def transform_stones_cache(counts, n_iter, current_iter \\ 0)
+  def transform_stones_cache(counts, n_iter, current_iter) when n_iter == current_iter, do: counts
 
-  def transform_stones([], n_iter, current_iter, acc) do
-    transform_stones(acc, n_iter, current_iter + 1)
+  def transform_stones_cache(counts, n_iter, current_iter) do
+    new_counts =
+      counts
+      |> Enum.reduce(%{}, fn {val, count}, acc ->
+        transform_stone(val)
+        |> Enum.reduce(acc, fn num, new_acc ->
+          Map.update(new_acc, num, count, fn old_count -> old_count + count end)
+        end)
+      end)
+
+    transform_stones_cache(new_counts, n_iter, current_iter + 1)
   end
 
-  def transform_stones(stones, n_iter, current_iter, _) when n_iter == current_iter,
-    do: stones
+  def init_stones_cache(stones) do
+    stones
+    |> Enum.reduce(%{}, fn stone, acc ->
+      Map.update(acc, stone, 1, fn count -> count + 1 end)
+    end)
+  end
 
-  def transform_stones([stone | rest], n_iter, current_iter, acc) do
-    case transform_stone(stone) do
-      [left, right] -> transform_stones(rest, n_iter, current_iter, [right, left | acc])
-      num -> transform_stones(rest, n_iter, current_iter, [num | acc])
-    end
+  def get_size(counts) do
+    counts
+    |> Enum.reduce(0, fn {_, count}, acc -> count + acc end)
   end
 
   def solve_1(file_path) do
     file_path
     |> read_file()
-    |> transform_stones(@n_blinks_1)
-    |> length()
+    |> init_stones_cache()
+    |> transform_stones_cache(@n_blinks_1)
+    |> get_size()
+  end
+
+  def solve_2(file_path) do
+    file_path
+    |> read_file()
+    |> init_stones_cache()
+    |> transform_stones_cache(@n_blinks_2)
+    |> get_size()
   end
 end
