5b9245ac56a81a3381d57aac8550394fce2751a3
Miłosz Bargieł <miloszbargiel@gmail.com>
2024-12-27 12:47:32 +0100

    Day 19 solution

--- day19.ex
+++ day19.ex
@@ -1,11 +1,11 @@
 defmodule Solution do
-  def read_file(path) do
+  defp read_file(path) do
     path
     |> File.read!()
     |> String.split("\n\n")
   end
 
-  def parse_input([towels, patterns]) do
+  defp parse_input([towels, patterns]) do
     towels =
       for towel <- String.split(towels, ", ", trim: true), reduce: MapSet.new() do
         acc -> MapSet.put(acc, towel)
@@ -19,53 +19,37 @@ defmodule Solution do
     {towels, patterns}
   end
 
-  def init_table(sequence) do
+  defp init_table(sequence) do
     len = String.length(sequence)
 
     table =
       1..len
-      |> Enum.reduce(%{}, fn n, acc -> Map.put(acc, n, false) end)
-      |> Map.put(0, true)
+      |> Enum.reduce(%{}, fn n, acc -> Map.put(acc, n, 0) end)
+      |> Map.put(0, 1)
 
     {table, len}
   end
 
-  def validate_sequence(sequence, len, table, towels, idx \\ 0)
-  def validate_sequence(_, len, table, _, idx) when len + 1 == idx, do: table[len]
+  defp validate_sequence(sequence, len, table, towels, idx \\ 0)
+  defp validate_sequence(_, len, table, _, idx) when len + 1 == idx, do: table[len]
 
-  def validate_sequence(sequence, len, table, towels, idx) do
-    table =
-      0..idx
-      |> Enum.reduce_while(table, fn n, acc ->
-        substr = String.slice(sequence, n..(idx - 1)//1)
-
-        if acc[n] and substr in towels do
-          {:halt, Map.put(acc, idx, true)}
-        else
-          {:cont, acc}
-        end
-      end)
-
-    validate_sequence(sequence, len, table, towels, idx + 1)
-  end
-
-  def validate_sequence(sequence, len, table, towels, idx) do
+  defp validate_sequence(sequence, len, table, towels, idx) do
     table =
       0..idx
       |> Enum.reduce(table, fn n, acc ->
         substr = String.slice(sequence, n..(idx - 1)//1)
 
-        if acc[n] and substr in towels do
-          Map.put(acc, idx, true)
+        if acc[n] != 0 and substr in towels do
+          Map.update(acc, idx, nil, &(&1 + Map.get(acc, n)))
         else
-          {:cont, acc}
+          acc
         end
       end)
 
     validate_sequence(sequence, len, table, towels, idx + 1)
   end
 
-  def solve_1(path) do
+  def solve(path) do
     {towels, patterns} =
       path
       |> read_file()
@@ -76,6 +60,8 @@ defmodule Solution do
       {table, len} = init_table(pattern)
       validate_sequence(pattern, len, table, towels)
     end)
-    |> Enum.reduce(0, fn {:ok, res}, acc -> if res, do: acc + 1, else: acc end)
+    |> Enum.reduce({0, 0}, fn {:ok, res}, {valid, n_patterns} ->
+      if res != 0, do: {valid + 1, n_patterns + res}, else: {valid, n_patterns}
+    end)
   end
 end
